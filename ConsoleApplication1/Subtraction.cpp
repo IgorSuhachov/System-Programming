69afee74ba03ecf5a8c5483017451fbec7148682
IgorSuhachov <42006386+IgorSuhachov@users.noreply.github.com>
2020-02-20 17:21:21 +0300

    Update Subtraction.cpp

--- ConsoleApplication1/Subtraction.cpp
+++ ConsoleApplication1/Subtraction.cpp
@@ -23,4 +23,7 @@ void Subtraction() {
 
 	printf("\nРезультат:");
 	printf("\n%d - %d = %d", a, b, subt);
+	cout << "\nНажмите любую клавишу для перехода в меню" << endl;
+	system("pause");
+	system("cls");
 }
