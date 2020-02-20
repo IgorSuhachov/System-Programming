973a03ff7a0b0ea448e67c4909a201fa1a32cda1
IgorSuhachov <42006386+IgorSuhachov@users.noreply.github.com>
2020-02-20 17:21:18 +0300

    Update Multiplication.cpp

--- ConsoleApplication1/Multiplication.cpp
+++ ConsoleApplication1/Multiplication.cpp
@@ -24,4 +24,7 @@ void Multiplication() {
 
 	printf("\nРезультат:");
 	printf("\n%d * %d = %d", a, b, mult);
+	cout << "\nНажмите любую клавишу для перехода в меню" << endl;
+	system("pause");
+	system("cls");
 }
