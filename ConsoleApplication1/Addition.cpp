9768307c9039758e539478759cedd7c5514db9c6
IgorSuhachov <42006386+IgorSuhachov@users.noreply.github.com>
2020-02-23 21:24:04 +0300

    Update Addition.cpp

--- ConsoleApplication1/Addition.cpp
+++ ConsoleApplication1/Addition.cpp
@@ -23,10 +23,4 @@ void Addition() {
 
 	printf("\nРезультат:");
 	printf("\n%d + %d = %d", a, b, sum);
-
-	cout << "\nНажмите любую клавишу для перехода в меню" << endl;
-
-	system("pause");
-
-	system("cls");
 }
