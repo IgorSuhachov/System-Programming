c368bcdb8f34a0f10c454e03303963b739235ff9
IgorSuhachov <42006386+IgorSuhachov@users.noreply.github.com>
2020-02-23 21:24:18 +0300

    Update Main.cpp

--- ConsoleApplication1/Main.cpp
+++ ConsoleApplication1/Main.cpp
@@ -1,61 +1,101 @@
 ﻿#include <iostream>
+#include <string>
+#include <windows.h>
 
 using namespace std;
 
 int main() {
-	system("color 2");
-
 	setlocale(LC_ALL, "rus");
-	
-	int answer;
+
+	int answer{ 0 };
 
 	void Addition(), Subtraction();
 	void Division(), Multiplication();
 	void BigMultiplication();
 	void task1(), task2();
-	void Menu();
-	
-	while(true) {
+	void Menu(), Initials();
+
+	while (true) {
+		system("cls");
+
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 2);
+
+		Initials();
+
 		Menu();
 
-		cin >> answer;
-		switch (answer) {
-		case 1:
-			Addition();
-			break;
+		for (int i = 0; i < 9; ++i) {
+			string menu[9] = { "| \t1. Сложение \t\t\t|","| \t2. Вычитание \t\t\t|","| \t3. Умножение \t\t\t|","| \t4. Деление \t\t\t|","| \t5. Умножение больших чисел \t|","| \t6. Практическая 1 \t\t|","| \t7. Практическая 2 \t\t| ","| \t0. Выход \t\t\t|"," " };
+			cout << endl;
 
-		case 2:
-			Subtraction();
-			break;
-
-		case 3:
-			Multiplication();
-			break;
-
-		case 4:
-			Division();
-			break;
-
-		case 5:
-			BigMultiplication();
-			break;
-
-		case 6:
-			task1();
-			break;
-
-		case 7:
-			task2();
-			break;
-
-		case 0:
-			exit(0);
-			break;
-
-		default:
-			cout << "Неверный пункт меню" << endl;
+			if (i == answer) {
+				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 3);
+				cout << menu[i];
+			} else {
+				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 2);
+				cout << menu[i];
+			}
 		}
-	}
 
-	return 0;
+		while (true) {
+			if (GetAsyncKeyState(VK_UP) != 0) {
+				answer -= 1;
+
+				if (answer == -1)
+					answer = 9;
+				break;
+
+			} else if (GetAsyncKeyState(VK_DOWN) != 0) {
+				answer += 1;
+
+				if (answer == 9)
+					answer = 0;
+				break;
+
+			} else if (GetAsyncKeyState(VK_RETURN) != 0) {
+				switch (answer) {
+				case 1:
+					Addition();
+					break;
+
+				case 2:
+					Subtraction();
+					break;
+
+				case 3:
+					Multiplication();
+					break;
+
+				case 4:
+					Division();
+					break;
+
+				case 5:
+					BigMultiplication();
+					break;
+
+				case 6:
+					task1();
+					break;
+
+				case 7:
+					task2();
+					break;
+
+				case 8:
+					exit(0);
+					break;
+					return 0;
+
+				case 9:
+					break;
+				}
+			}
+		}
+
+		if (answer !=9)
+			system("pause");
+
+		Sleep(150);
+	}
 }
