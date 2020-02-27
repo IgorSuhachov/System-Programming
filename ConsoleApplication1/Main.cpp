eaee23cf672bf556dc9427d489d961a9e3c749d3
IgorSuhachov <42006386+IgorSuhachov@users.noreply.github.com>
2020-02-27 20:04:58 +0300

    Update Main.cpp

--- ConsoleApplication1/Main.cpp
+++ ConsoleApplication1/Main.cpp
@@ -8,11 +8,9 @@ int main() {
 
 	int answer{ 0 };
 
-	void Addition(), Subtraction();
-	void Division(), Multiplication();
-	void BigMultiplication();
 	void task1(), task2();
 	void Menu(), Initials();
+	void Calculator(), Power();
 
 	while (true) {
 		system("cls");
@@ -23,8 +21,9 @@ int main() {
 
 		Menu();
 
-		for (int i = 0; i < 9; ++i) {
-			string menu[9] = { "| \t1. Сложение \t\t\t|","| \t2. Вычитание \t\t\t|","| \t3. Умножение \t\t\t|","| \t4. Деление \t\t\t|","| \t5. Умножение больших чисел \t|","| \t6. Практическая 1 \t\t|","| \t7. Практическая 2 \t\t| ","| \t0. Выход \t\t\t|"," " };
+		for (int i = 0; i < 5; ++i) {
+			string menu[5] = { "| \t1. Калькулятор \t\t|","| \t2. Практическая 1 \t|","| \t3. Практическая 2 \t|",
+				"| \t4. Возведение в степень |","| \t5. Выход \t\t|"};
 			cout << endl;
 
 			if (i == answer) {
@@ -41,51 +40,36 @@ int main() {
 				answer -= 1;
 
 				if (answer == -1)
-					answer = 9;
+					answer = 5;
 				break;
 
 			} else if (GetAsyncKeyState(VK_DOWN) != 0) { // control Down
 				answer += 1;
 
-				if (answer == 9)
+				if (answer == 5)
 					answer = 0;
 				break;
 
 			} else if (GetAsyncKeyState(VK_RETURN) != 0) { // control Enter
 				switch (answer) {
 				case 1:
-					Addition();
+					Calculator();
 					break;
 
 				case 2:
-					Subtraction();
-					break;
-
-				case 3:
-					Multiplication();
-					break;
-
-				case 4:
-					Division();
-					break;
-
-				case 5:
-					BigMultiplication();
-					break;
-
-				case 6:
 					task1();
 					break;
 
-				case 7:
+				case 3:
 					task2();
 					break;
 
-				case 8:
-					exit(0);
+				case 4:
+					Power();
 					break;
 
-				case 9:
+				case 5:
+					exit(0);
 					break;
 				}
 			}
