ff1959b49fcddf715f453b391badf379df74622b
IgorSuhachov <42006386+IgorSuhachov@users.noreply.github.com>
2020-03-05 22:36:10 +0300

    Update task1.cpp

--- ConsoleApplication1/task1.cpp
+++ ConsoleApplication1/task1.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
+#include <bitset>
 
 using namespace std;
 
 void task1() {
-	cout << "\n\nПрактическая 1" << endl;
+	short a, s;
+
+	cout << "\n\n\tВариант 9\n\n";
+
+	cout << "Пусть а – однобайтное беззнаковое число\n";
+	cout << "Установить в «1» второй и восьмой биты этого числа.\n";
+	cout << "Вывести полученное число на экран.\n";
+
+	cout << "\n\nВведите а:";
+	cin >> a;
+
+	cout << bitset<sizeof(a) * 4>(a) << endl;
+
+	_asm {
+		xor eax, eax
+
+		mov ax, a
+		or ax, 01000001b
+	
+		mov s, ax
+	}
+
+	cout << bitset<sizeof(s) * 4>(s) << endl;
+
 	system("pause");
 }
